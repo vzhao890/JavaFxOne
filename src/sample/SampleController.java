c61fb905fdc389b54c474d9ccef9fc47d46b9f89
BT_1N3_01@bktech.local <BT_1N3_01@bktech.local>
2018-12-12 09:31:28 -0500

    Initial commit

---
+++ src/sample/SampleController.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class SampleController
+{
+
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent)
+    {
+        helloWorld.setText("Hello World!");
+    }
+    public void add(ActionEvent actionEvent)
+    {
+        helloWorld.setText("Hello World!");
+    }
+    public void subtract(ActionEvent actionEvent)
+    {
+        helloWorld.setText("Hello World!");
+    }
+    public void addFive(ActionEvent actionEvent)
+    {
+        helloWorld.setText("Hello World!");
+    }
+    public void subtractFive(ActionEvent actionEvent)
+    {
+        helloWorld.setText("Hello World!");
+    }
+
+}
